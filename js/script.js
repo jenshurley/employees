b9980bd5d069d3864dc6458ab7afa37fcd3fa4a8
Jen Shurley <jenshurley@gmail.com>
2014-11-13 19:35:34 -0700

    added slideUp and slideDown to edit_box, bootstrap styling for buttons and resizing input elements

--- js/script.js
+++ js/script.js
@@ -36,15 +36,15 @@ $(document).ready(function(){
 
     $("#add").click(function() {
          console.log('clicked!');
-        $('#edit_box').slideUp(render_edit_box('add')).show();
-        //slide still not happening
-        // render_edit_box('add');
+        $('#edit_box').slideDown(render_edit_box('add')).show();
+
+
     });
 
 
 
 });
-//what goes in the document.ready function?
+
 
 function delete_employee(index){
     console.log(index);
@@ -75,8 +75,11 @@ function render_edit_box(type, employee, index){
     }
 
 
-    var html = ' <div class="input-group"><input type="text" class="form-control" placeholder="name"></div><div><label>name</label><input id="edit_name" value="' + pre_name + '"></div><div><label>phone</label> <input id="edit_phone" value="'+ pre_phone + '"></div><div><label>address</label> <input id="edit_address" value="'+ pre_address + '"></div>';
-    //TODO make input box for address wider. label above field. Need display to be block.?
+    var html = '<div class="input-group"><label for="whatever">Name<input type="text" class="form-control" placeholder="name" id="edit_name" value="' + pre_name + '"></label>' +
+        '<div class="input-group"><label for="whatever">Phone<input type="text" class="form-control" placeholder="phone number" id="edit_phone" value="'+ pre_phone + '"></label></div>' +
+        '<div class="input-group"><label for="whatever">Address<input type="text" class="form-control" placeholder="address"id="edit_address" value="'+ pre_address + '"></label>' +'<div>';
+    //TODO make input box for address wider.
+    //TODO
 
     var button_name = type == "add" ? "add it" : "update it"; 
 
@@ -99,7 +102,7 @@ function render_edit_box(type, employee, index){
             update_employee(e, index);
         }
 
-         $("#edit_box").html('');
+         $("#edit_box").html('').slideUp();
 
     })
 
@@ -128,6 +131,18 @@ function add_employee(data){
 
 }
 
+//var data =
+//    jQuery.parseJSON;
+//
+//    $.ajax({
+//                type: "Get",
+//                url: "http://69.164.197.6/employees",
+//                data: { name: "John",: "Boston" }
+//            })
+//                .done(function( msg ) {
+//                    console.log( "Data Saved: " + msg );
+//            });
+//            return
 
 function render_employee_table(data){
 
@@ -144,8 +159,8 @@ function render_employee_table(data){
         html += "<td>"+ employee.name +"</td>";
         html += "<td>"+ employee.phone +"</td>";
         html += "<td>"+ employee.address +"</td>";
-        html += "<td><button class='delete' index='"+index+"'>Del</button></td>";
-        html += "<td><button index="+ index +" class='edit'>Edit</button></td>";
+        html += "<td><button type='button' class='btn btn-default btn-sm delete' index='"+index+"'>Del</button></td>";
+        html += "<td><button type='button'' class='btn btn-default btn-sm' index='"+ index +" class='edit'>Edit</button></td>";
 
         html += "</tr>";
     })
@@ -165,8 +180,6 @@ function render_employee_table(data){
 
 
          render_edit_box('edit', employees[$(this).attr("index")] , $(this).attr("index"));
-   
-
 
     })
 
